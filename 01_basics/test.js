aaad49fae9ed89a069c9f0a592bace3f701d5f24
HarshBhagat <116473834+harsh0717666@users.noreply.github.com>
2024-12-23 07:11:41 +0000

    container config done

--- 01_basics/test.js
+++ 01_basics/test.js
@@ -1,1 +1,9 @@
-console.log("Harsh");
+const accountId = 1234
+let accountEmail = "harsh@gmail.com"
+var accountPassword = "11111"
+accountCity = "vid"
+
+accountEmail= "hb@gmail.com"
+accountPassword= "22222"
+accountCity = "bpl"
+ console.table([ accountEmail,accountPassword,accountCity,accountId])
