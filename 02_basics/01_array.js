7986ec1f63ab507d65e467f21adb5570f8d4ee0f
HarshBhagat <116473834+harsh0717666@users.noreply.github.com>
2024-12-25 10:04:06 +0000

    container config done

---
+++ 02_basics/01_array.js
@@ -0,0 +1,14 @@
+let arr= [1,3,2,34,5,55,6,]
+
+// console.log(arr.push);
+// array method 
+// arr.push(4)
+// console.log(arr);
+// arr.pop()
+// console.log(arr);
+// arr.unshift(111)
+// console.log(arr);
+// arr.shift()
+// console.log(arr);
+
+console.log(arr.indexOf(55));
---
+++ 02_basics/02_object.js
@@ -0,0 +1,39 @@
+// const info= {
+//     name :"Harsh bhagat ",
+//     Age :21,
+//     Location:"pune",
+//     Isactive:"true",
+// }
+// console.log(info);
+// console.log(info["name"]);
+// ***********Function********************
+// function online (username){
+//     return `${username} online right now`
+// }
+//   console.log(online("Harsh"));
+
+
+
+
+//************* arrow function *******************************/
+
+
+//  function info (){
+//     let username = "harsh bhagat"
+//     age =21
+//     salary= 50000
+    
+    
+//  }
+//  console.log(info);
+ 
+// function addnum (num1,num2){
+// return (num1+num2)
+// }
+// console.log(addnum(3,4));
+  
+
+const addnum = (num1,num2)=>{
+return(num1+num2)
+}
+console.log(addnum(2,3));
